f5b73d6949283ebb3a10cb0c2a2a5cbc26439a8f
Omega-19 <vigannicole1@gmail.com>
2023-07-04 18:02:00 +0100

    my add

---
+++ Desktop/bureau_dossiers/les_objets_programmation/objets_classes_js/request.js
@@ -0,0 +1,10 @@
+`use strict`
+ class Request{
+    getInputs(inputs){
+        for (let i = 0; i < inputs.length; i++) {
+           console.log(inputs[i].value);
+            
+        }
+    }
+}
+export { Request };
